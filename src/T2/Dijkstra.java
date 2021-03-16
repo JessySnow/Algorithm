a9e4acb844b1558b2bc53105b855bcadf7114e56
Jessy Snow <61576653+JessySnow@users.noreply.github.com>
2021-03-16 22:07:08 +0800

    Completely finish!
    所有样例改为从定义的数组中读取

--- src/T2/Dijkstra.java
+++ src/T2/Dijkstra.java
@@ -1,6 +1,58 @@
 package T2;
-import java.util.Scanner;
 
-public class Dijkstra {
+/*
+* description:求从点a到点h的最短距离
+* */
+public class Dijkstra{
+    private static int INF = Integer.MAX_VALUE;
+    private static int[][] Matrix= {
+            {0, 1, INF, INF, INF, INF, INF, INF},
+            {INF, 0, INF, 2, INF, INF, INF, INF},
+            {2, INF, 0, INF, INF, INF, INF, INF},
+            {INF, INF, 1, 0, INF, 8, INF, INF},
+            {INF, INF, INF, 2, 0, INF, 2, INF},
+            {INF, INF, INF, INF, 2, 0, INF, INF},
+            {INF, INF, INF, INF, INF, 3, 0, 3},
+            {INF, INF, INF, INF, INF, 2, INF, 0}
+    };
+    public static void main(String[] args) {
+        int size = Matrix.length;
+        int startP = 0;
+        int endP = 7;
+        int[] shortest = new int[size];     //最短距离数组
+        int[] visited = new int[size];      //已经访问的数组
 
+        //初始化数组
+        shortest[startP] = 0;
+        visited[startP] = 1;
+
+        //Dijkstra算法代码
+        for (int i = 1; i < size; i ++){
+            int k = -1;
+            int min = INF;
+
+            //从未访问的节点中选出距离当前节点距离最短的节点
+            for (int j = 1; j < size; j ++)
+                if (visited[j] == 0 && Matrix[startP][j] < min){
+                    min = Matrix[startP][j];
+                    k = j;
+                }
+
+            //更新访问数组和最短距离数组
+            visited[k] = 1;
+            shortest[k] = min;
+
+
+            //更新从起始点开始的距离表
+            for (int index = 1; index < size; index ++){
+                if (visited[index] == 0){
+                    //防止数据溢出
+                    int tmpLength = (Matrix[startP][k] == INF || Matrix[k][index] == INF) ? INF:Matrix[startP][k] + Matrix[k][index];
+                    Matrix[startP][index] = Math.min(tmpLength, Matrix[startP][index]);
+                }
+            }
+        }
+
+        System.out.println("从a到h的最短路径为：" + shortest[endP]);
+    }
 }
--- src/T2/Floyd.java
+++ src/T2/Floyd.java
@@ -1,56 +1,30 @@
 package T2;
-import java.util.Scanner;
-/*
-* description: 定义距离矩阵
-* */
-class disMatrix{
-    public int size;
-    public int[][] weight;
-    private final int INF = Integer.MAX_VALUE;
-    public void trans(int[][] Matrix, int size){
-        for(int i = 0; i < size; i ++)
-            for(int j = 0; j < size; j ++)
-                if(Matrix[i][j] == 0 && i != j) Matrix[i][j] = INF;
-    }
-}
 
 public class Floyd {
+    private static final int INF = Integer.MAX_VALUE;   //用INF来代表两个点直接没有直接相连的路径
+    //有向图的邻接矩阵和距离矩阵
+    private static int[][] adjMatrix = {{0, 2, 6, 4},
+                                        {INF, 0, 3, INF},
+                                        {7, INF, 0, 1},
+                                        {5, INF, 12, 0}},
+                                        Distance = adjMatrix;
     public static void main(String[] args) {
-        Scanner keyIn = new Scanner(System.in);
-        disMatrix diM = new disMatrix();
-        System.out.println("输入点的个数");
-        diM.size = keyIn.nextInt();
-        diM.weight = new int[diM.size][diM.size];
-        int[][] distanceM = new int[diM.size][diM.size];
+        int size = adjMatrix.length;
 
-        System.out.println("输入权值矩阵，不相连的用0代替");
-        for (int i = 0; i < diM.size; i ++)
-            for (int j = 0; j < diM.size; j ++)
-                diM.weight[i][j] = keyIn.nextInt();
-        diM.trans(diM.weight, diM.size);
+        //Floyd核心代码
+        for (int k = 0; k < size; k ++)
+            for (int i = 0; i < size; i ++)
+                for (int j = 0; j < size; j ++){
+                    //防止溢出
+                    int tmp = (Distance[i][k] == INF || Distance[k][j] == INF) ? INF : (Distance[i][k] + Distance[k][j]);
+                    Distance[i][j] = Math.min(tmp, Distance[i][j]);
+                }
 
-        for (int k = 0; k < diM.size; k ++)
-            for (int i = 0; i < diM.size; i ++)
-                for (int j = 0; j < diM.size; j ++)
-                    distanceM[i][j] = Math.min(diM.weight[i][j], diM.weight[j][k] + diM.weight[k][j]);
-
-        for (int i = 0; i < diM.size; i ++)
-            for (int j = 0; j < diM.size; j ++)
-                if(j != diM.size - 1) System.out.print(distanceM[i][j] + " ");
-                else                  System.out.println(distanceM[i][j]);
+        //输出路径矩阵
+       for (int i = 0; i < size; i++)
+           for (int j = 0; j < size; j ++){
+               if(j != size - 1)    System.out.print(Distance[i][j] + " ");
+               else                 System.out.println(Distance[i][j]);
+           }
     }
 }
-
-/*
-* 输入样例:距离矩阵
-* 节点： 1， 2， 3， 4*/
-/*
-0 2 6 4
-0 0 3 0
-6 0 0 1
-5 0 12 0
-* */
-
-/*
-输出：距离矩阵
-* */
